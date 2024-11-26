12b1dc2eb1e759cb5b412764b84ef2734bf3aa76
Juan perri <111884460+perrijuan@users.noreply.github.com>
2024-11-26 01:02:38 -0300

    Add files via upload

---
+++ prefixsum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    vector<int> query;
+
+    for(int i =0;i<n;i++){
+        int x;
+        cin>>x;
+        query.push_back(x);
+    }
+
+    vector<int>prefixsum(n,0);
+
+    prefixsum[0] = query[0];
+
+    for(int a=0;a<n;a++){
+        prefixsum[a]=prefixsum[a-1]+query[a];
+        
+    }
+
+    for(auto run : prefixsum){
+        cout<<run<<" ";
+    }
+}
